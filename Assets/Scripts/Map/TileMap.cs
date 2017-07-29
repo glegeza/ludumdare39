ccfaf66de6c32ab6d8015124f3190f81a0a1e860
George Legeza <glegeza@gmail.com>
2017-07-29 14:37:53 -0400

    Add method for getting tile coordinates

--- Assets/Scripts/Map/TileMap.cs
+++ Assets/Scripts/Map/TileMap.cs
@@ -1,11 +1,10 @@
 ﻿namespace DLS.LD39.Map
 {
-    using System.Collections;
-    using System.Collections.Generic;
     using UnityEngine;
     using Utility.Unity.ProceduralMeshes;
 
     [RequireComponent(typeof(MeshFilter))]
+    [RequireComponent(typeof(BoxCollider2D))]
     public class TileMap : MonoBehaviour
     {
         public int Width;
@@ -13,18 +12,43 @@
         public Vector2 TileSize;
 
         private MeshFilter _filter;
+        private BoxCollider2D _collider;
 
-        // Use this for initialization
-        void Start()
+        public Vector2 WorldSize
         {
-            _filter = GetComponent<MeshFilter>();
-            GetMesh();
+            get; private set;
+        }
+        
+        public bool GetTileCoords(Vector2 worldCoords, out float x, out float y)
+        {
+            x = y = -1;
+            var localCoords = transform.InverseTransformPoint(worldCoords);
+
+            // Translate so we're starting from the upper left corner
+            var halfWidth = WorldSize.x / 2.0f;
+            var halfHeight = WorldSize.y / 2.0f;
+            localCoords += new Vector3(halfWidth, halfHeight, 0.0f);
+
+            x = (int)localCoords.x;
+            y = (int)(Height - localCoords.y);
+
+            if (localCoords.x < 0.0f || localCoords.x > WorldSize.x ||
+                localCoords.y < 0.0f || localCoords.y > WorldSize.y)
+            {
+                return false;
+            }
+            
+            return true;
         }
 
-        // Update is called once per frame
-        void Update()
+        private void Start()
         {
-
+            _filter = GetComponent<MeshFilter>();
+            _collider = GetComponent<BoxCollider2D>();
+            GetMesh();
+            WorldSize = new Vector2(Width * TileSize.x,
+                Width * TileSize.y);
+            _collider.size = WorldSize;
         }
 
         private void GetMesh()
