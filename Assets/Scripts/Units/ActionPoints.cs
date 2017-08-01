b7b8270f77f8cf3bccde668a8e141fa13ed355ec
George Legeza <glegeza@gmail.com>
2017-08-01 15:58:01 -0400

    Add AP class

---
+++ Assets/Scripts/Units/ActionPoints.cs
@@ -0,0 +1,44 @@
+﻿namespace DLS.LD39.Units
+{
+    using UnityEngine;
+
+    class ActionPoints : MonoBehaviour
+    {
+        public int PointsPerTurn
+        {
+            get; set;
+        }
+
+        public int PointsRemaining
+        {
+            get; set;
+        }
+
+        public int MaximumPoints
+        {
+            get; set;
+        }
+
+        public void BeginTurn()
+        {
+            PointsRemaining += PointsPerTurn;
+            PointsRemaining = Mathf.Min(PointsRemaining, MaximumPoints);
+        }
+
+        public void EndTurn()
+        {
+
+        }
+
+        public void SpendPoints(int amount)
+        {
+            if (amount > PointsRemaining)
+            {
+                Debug.LogError("Unit attempting to spend too many points");
+                return;
+            }
+
+            PointsRemaining -= amount;
+        }
+    }
+}
