a5e5d624a18cea5027d2ebd085a005409288c3de
George Legeza <glegeza@gmail.com>
2017-08-01 01:50:54 -0400

    Stub out some pathfinding shit

---
+++ Assets/Scripts/Pathfinding/SimplePathfinder.cs
@@ -0,0 +1,33 @@
+﻿namespace DLS.LD39.Pathfinding
+{
+    using DLS.LD39.Map;
+    using System.Collections.Generic;
+    using UnityEngine;
+
+    class SimplePathfinder
+    {
+        public List<Node> GetPath(Tile start, Tile target)
+        {
+            var path = new List<Node>();
+
+
+
+            return path;
+        }
+
+        private int GetGValue(Node parent, Tile candidate)
+        {
+            var parentTile = parent.NodeTile;
+            return parent.G + (parentTile.X == candidate.X || parentTile.Y == candidate.Y
+                ? 10
+                : 14);
+        }
+
+        private int GetHValue(Tile candidate, Tile target)
+        {
+            var xDis = Mathf.Abs(target.X - candidate.X);
+            var yDis = Mathf.Abs(target.Y - candidate.Y);
+            return (xDis + yDis) * 10;
+        }
+    }
+}
