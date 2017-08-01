51b61a81d06a2ab1d12177d80bb5ac5b54cffd3b
George Legeza <glegeza@gmail.com>
2017-08-01 15:58:11 -0400

    Add game unit class

---
+++ Assets/Scripts/Units/GameUnit.cs
@@ -0,0 +1,57 @@
+﻿namespace DLS.LD39.Units
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using UnityEngine;
+
+    [RequireComponent(typeof(ActionPoints))]
+    [RequireComponent(typeof(Initiative))]
+    [RequireComponent(typeof(MoveToTile))]
+    class GameUnit : MonoBehaviour
+    {
+        public event EventHandler<EventArgs> TurnEnded;
+
+        public ActionPoints AP
+        {
+            get; private set;
+        }
+
+        public Initiative Init
+        {
+            get; private set;
+        }
+
+        public MoveToTile MoveController
+        {
+            get; private set;
+        }
+
+        public void BeginTurn()
+        {
+            Init.BeginTurn();
+            AP.BeginTurn();
+            MoveController.BeginTurn();
+        }
+
+        public void EndTurn()
+        {
+            Init.EndTurn();
+            AP.EndTurn();
+            MoveController.EndTurn();
+
+            if (TurnEnded != null)
+            {
+                TurnEnded(this, EventArgs.Empty);
+            }
+        }
+
+        private void Start()
+        {
+            AP = GetComponent<ActionPoints>();
+            Init = GetComponent<Initiative>();
+            MoveController = GetComponent<MoveToTile>();
+        }
+    }
+}
