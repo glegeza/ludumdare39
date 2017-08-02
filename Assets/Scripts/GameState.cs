c87e7a227f82fd51c799a17202cca184241b2534
George Legeza <glegeza@gmail.com>
2017-08-02 15:22:47 -0400

    Add stubbed GameState singleton

---
+++ Assets/Scripts/GameState.cs
@@ -0,0 +1,27 @@
+﻿namespace DLS.LD39
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using UnityEngine;
+
+    class GameState : MonoBehaviour
+    {
+        private static GameState _instance;
+
+        public static GameState Instance { get { return _instance; } }
+
+        private void Awake()
+        {
+            if (_instance != null && _instance != this)
+            {
+                Destroy(gameObject);
+            }
+            else
+            {
+                _instance = this;
+            }
+        }
+    }
+}
