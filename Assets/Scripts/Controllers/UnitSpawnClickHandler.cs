0cd3c55b10b1e6fb7d67ff3dae8ee636de6b2273
George Legeza <glegeza@gmail.com>
2017-08-02 15:25:24 -0400

    Add click handler for spawning units

---
+++ Assets/Scripts/Controllers/UnitSpawnClickHandler.cs
@@ -0,0 +1,24 @@
+﻿namespace DLS.LD39.Controllers
+{
+    using DLS.LD39.Map;
+
+    class UnitSpawnClickHandler : IMapClickInputHandler
+    {
+        public bool HandleTileClick(int button, Tile clickedTile)
+        {
+            if (button == 0)
+            {
+                UnitSpawner.Instance.SpawnTestUnit(clickedTile);
+            }
+            else if (button == 1)
+            {
+                var unit = UnitSpawner.Instance.UnitAtTile(clickedTile);
+                if (unit != null)
+                {
+                    UnitSpawner.Instance.RemoveUnit(unit);
+                }
+            }
+            return false;
+        }
+    }
+}
