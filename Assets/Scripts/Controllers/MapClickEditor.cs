39765e8ded25e9bca29d1094a72a9b6e73854b3f
George Legeza <glegeza@gmail.com>
2017-08-02 15:24:40 -0400

    Change MapClickEditor to an IMapClickInputHandler

---
+++ Assets/Scripts/Controllers/MapClickEditor.cs
@@ -0,0 +1,28 @@
+﻿namespace DLS.LD39.Controllers
+{
+    using DLS.LD39.Map;
+
+    class MapClickEditor : IMapClickInputHandler
+    {
+        public bool HandleTileClick(int button, Tile targetTile)
+        {
+            if (button != 0)
+            {
+                return false;
+            }
+
+            if (targetTile.Passable)
+            {
+                targetTile.Passable = false;
+                targetTile.Map.SetTileAt(targetTile.X, targetTile.Y, 1);
+            }
+            else
+            {
+                targetTile.Passable = true;
+                targetTile.Map.SetTileAt(targetTile.X, targetTile.Y, 0);
+            }
+
+            return false;
+        }
+    }
+}
