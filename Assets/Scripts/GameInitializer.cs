18e64b063590828ee52209e44df2569655cdf7cb
George Legeza <glegeza@gmail.com>
2017-08-02 15:25:47 -0400

    Add GameInitializer class to handle singleton initialization

---
+++ Assets/Scripts/GameInitializer.cs
@@ -0,0 +1,39 @@
+﻿namespace DLS.LD39
+{
+    using UnityEngine;
+    using Controllers;
+
+    /// <summary>
+    /// Responsible for setting up all of the other persistent game
+    /// managers, controllers, etc.
+    /// </summary>
+    class GameInitializer : MonoBehaviour
+    {
+        private static GameInitializer _instance;
+
+        public static GameInitializer Instance
+        {
+            get
+            {
+                return _instance;
+            }
+        }
+
+        private void Awake()
+        {
+            if (_instance != null && _instance != this)
+            {
+                Destroy(gameObject);
+            }
+            else
+            {
+                _instance = this;
+            }
+        }
+
+        private void Start()
+        {
+            MapClickController.Instance.Initialize();
+        }
+    }
+}
