775fcbc8e9e53aa8fb97ea5ed0aaaced053b5a3b
George Legeza <glegeza@gmail.com>
2017-08-03 18:52:40 -0400

    Add classes to simplify dealing with these strip tile sheets

---
+++ Assets/Scripts/Map/IndexedTile.cs
@@ -0,0 +1,39 @@
+﻿namespace DLS.LD39.Map
+{
+    using UnityEngine;
+
+    class IndexedTile
+    {
+        public IndexedTile(TileSheet sheet, int idx, Vector2 bottomLeft, float width, float height)
+        {
+            BottomLeft = bottomLeft;
+            Width = width;
+            Height = height;
+        }
+
+        public TileSheet Sheet
+        {
+            get; private set;
+        }
+
+        public int Index
+        {
+            get; private set;
+        }
+
+        public Vector2 BottomLeft
+        {
+            get; private set;
+        }
+
+        public float Width
+        {
+            get; private set;
+        }
+
+        public float Height
+        {
+            get; private set;
+        }
+    }
+}
