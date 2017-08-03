e687e7c9eb1aa7b97ac1778c8e8830e9009d825f
George Legeza <glegeza@gmail.com>
2017-08-03 14:25:39 -0400

    Add singleton to track the currently selected unit

---
+++ Assets/Scripts/ActiveSelectionTracker.cs
@@ -0,0 +1,52 @@
+﻿namespace DLS.LD39
+{
+    using System;
+    using UnityEngine;
+
+    class ActiveSelectionTracker : MonoBehaviour
+    {
+        private static ActiveSelectionTracker _instance;
+
+        public static ActiveSelectionTracker Instance
+        {
+            get { return _instance; }
+        }
+
+        public event EventHandler<EventArgs> SelectionChanged;
+
+        public GameObject SelectedObject
+        {
+            get; private set;
+        }
+
+        public void SetSelection(GameObject obj)
+        {
+            Debug.Log("Setting selection");
+            if (obj != SelectedObject)
+            {
+                SelectedObject = obj;
+                if (SelectionChanged != null)
+                {
+                    SelectionChanged(this, EventArgs.Empty);
+                }
+            }
+        }
+
+        public void SetSelection(Component comp)
+        {
+            SetSelection(comp.gameObject);
+        }
+
+        private void Awake()
+        {
+            if (_instance != null && _instance != this)
+            {
+                Destroy(gameObject);
+            }
+            else
+            {
+                _instance = this;
+            }
+        }
+    }
+}
