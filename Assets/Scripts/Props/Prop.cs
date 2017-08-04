cbb517a6f0b7c1c035b54b51d2f9966c37852805
George Legeza <glegeza@gmail.com>
2017-08-04 01:09:47 -0400

    Add simple prop class stub

---
+++ Assets/Scripts/Props/Prop.cs
@@ -0,0 +1,33 @@
+﻿namespace DLS.LD39.Props
+{
+    using DLS.LD39.Map;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using UnityEngine;
+
+    class Prop : MonoBehaviour
+    {
+        public PropLayer Layer;
+        public bool Passable;
+
+        private SpriteRenderer _renderer;
+
+        public TilePosition Position
+        {
+            get; private set;
+        }
+
+        private void Awake()
+        {
+            _renderer = GetComponent<SpriteRenderer>();
+            Position = GetComponent<TilePosition>();
+        }
+
+        private void Update()
+        {
+            _renderer.sortingOrder = (int)((transform.position.y * 100) * -1);
+        }
+    }
+}
