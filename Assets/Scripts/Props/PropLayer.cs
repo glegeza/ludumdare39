c601b1b01ba5f7b588804c5dca629e1f70881112
George Legeza <glegeza@gmail.com>
2017-08-04 01:09:30 -0400

    Add enum for prop layers

---
+++ Assets/Scripts/Props/PropLayer.cs
@@ -0,0 +1,13 @@
+﻿namespace DLS.LD39.Props
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    enum PropLayer
+    {
+        Floor,
+        Wall
+    }
+}
