c2925dfc4415f3d9afc086db994d7713e340cbde
George Legeza <glegeza@gmail.com>
2017-08-04 01:09:08 -0400

    Add abstract singleton class

---
+++ Assets/Scripts/SingletonComponent.cs
@@ -0,0 +1,31 @@
+﻿namespace DLS.LD39
+{
+    using UnityEngine;
+
+    public abstract class SingletonComponent<T> : MonoBehaviour where T : SingletonComponent<T>
+    {
+
+        // Singleton reference
+        private static T _instance;
+
+        public static T Instance
+        {
+            get
+            {
+                return _instance;
+            }
+        }
+
+        protected virtual void Awake()
+        {
+            if (_instance != null && _instance != this)
+            {
+                Destroy(gameObject);
+            }
+            else
+            {
+                _instance = (T)this;
+            }
+        }
+    }
+}
