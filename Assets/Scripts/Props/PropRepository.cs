dfc7797b1dc4903b5f8b6ee5e86c1b0ddf7e7ce9
George Legeza <glegeza@gmail.com>
2017-08-04 01:11:38 -0400

    Add hilariously simple prop repo

---
+++ Assets/Scripts/Props/PropRepository.cs
@@ -0,0 +1,13 @@
+﻿namespace DLS.LD39.Props
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using UnityEngine;
+
+    class PropRepository : SingletonComponent<PropRepository>
+    {
+        public Prop WallPrefab;
+    }
+}
