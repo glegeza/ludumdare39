2213e3c619b4619f1eba2018a0d9ece3305c80c3
George Legeza <glegeza@gmail.com>
2017-08-06 16:58:55 -0400

    Add simple component to show the current mode

---
+++ Assets/Scripts/Interface/MapClickRouterModeText.cs
@@ -0,0 +1,26 @@
+﻿namespace DLS.LD39.Interface
+{
+    using DLS.LD39.Controllers;
+    using System;
+    using UnityEngine;
+    using UnityEngine.UI;
+
+    [RequireComponent(typeof(Text))]
+    class MapClickRouterModeText : MonoBehaviour
+    {
+        private Text _text;
+
+        private void Start()
+        {
+            _text = GetComponent<Text>();
+        }
+
+        private void Update()
+        {
+            var mode = MapClickRouter.Instance.ActiveMode;
+            var modeName = mode == null ?
+                "None" : mode.Name;
+            _text.text = String.Format("Current Mode: {0}", modeName);
+        }
+    }
+}
