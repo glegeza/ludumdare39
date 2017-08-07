e22224b2d3da35ef133e44bede3537fd873fc71a
George Legeza <glegeza@gmail.com>
2017-08-07 15:31:30 -0400

    Add stubbed CombatManager

---
+++ Assets/Scripts/Combat/CombatManager.cs
@@ -0,0 +1,14 @@
+﻿namespace DLS.LD39.Combat
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using UnityEngine;
+
+    class CombatManager : SingletonComponent<CombatManager>
+    {
+
+    }
+}
