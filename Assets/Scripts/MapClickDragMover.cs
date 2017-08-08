5ab712d90ae0a03e2504780629b54d7a905bdaa2
George Legeza <glegeza@gmail.com>
2017-08-08 11:37:47 -0400

    Add keyboard camera controls

--- Assets/Scripts/MapClickDragMover.cs
+++ Assets/Scripts/MapClickDragMover.cs
@@ -1,15 +1,13 @@
 ﻿namespace DLS.LD39
 {
     using UnityEngine;
-    using System;
-    using System.Collections.Generic;
-    using System.Linq;
-    using System.Text;
 
     [RequireComponent(typeof(Camera))]
     class MapClickDragMover : MonoBehaviour
     {
         public float MoveSpeed = 3.0f;
+        [Range(0.0f, 10.0f)]
+        public float KeyboardModifier = 0.5f;
         public Vector2 LowerLeftBounds = new Vector2(-40.0f, -40.0f);
         public Vector2 UpperRightBounds = new Vector2(40.0f, 40.0f);
 
@@ -22,18 +20,44 @@
 
         private void Update()
         {
+            var x = 0.0f;
+            var y = 0.0f;
+
             if (Input.GetMouseButton(2))
             {
-                transform.position += new Vector3(
-                    -Input.GetAxisRaw("Mouse X") * Time.deltaTime * MoveSpeed,
-                    -Input.GetAxisRaw("Mouse Y") * Time.deltaTime * MoveSpeed,
-                    0.0f);
-
-                var x = Mathf.Clamp(transform.position.x, LowerLeftBounds.x, UpperRightBounds.x);
-                var y = Mathf.Clamp(transform.position.y, LowerLeftBounds.y, UpperRightBounds.y);
-
-                transform.position = new Vector3(x, y, transform.position.z);
+                x = -Input.GetAxisRaw("Mouse X");
+                y = -Input.GetAxisRaw("Mouse Y");
             }
+            else
+            { 
+                if (Input.GetKey(KeyCode.A))
+                {
+                    x += -1.0f * KeyboardModifier;
+                }
+                if (Input.GetKey(KeyCode.D))
+                {
+                    x += 1.0f * KeyboardModifier;
+                }
+                if (Input.GetKey(KeyCode.W))
+                {
+                    y += 1.0f * KeyboardModifier;
+                }
+                if (Input.GetKey(KeyCode.S))
+                {
+                    y += -1.0f * KeyboardModifier;
+                }
+            }
+
+            
+            transform.position += new Vector3(
+                x * Time.deltaTime * MoveSpeed,
+                y * Time.deltaTime * MoveSpeed,
+                0.0f);
+
+            var transX = Mathf.Clamp(transform.position.x, LowerLeftBounds.x, UpperRightBounds.x);
+            var transY = Mathf.Clamp(transform.position.y, LowerLeftBounds.y, UpperRightBounds.y);
+
+            transform.position = new Vector3(transX, transY, transform.position.z);
         }
     }
 }
