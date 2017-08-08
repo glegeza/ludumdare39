45f8af81bb71c91a0f259646c441226a8f40714b
George Legeza <glegeza@gmail.com>
2017-08-08 12:26:26 -0400

    Change prop placer to place the selected prop type

--- Assets/Scripts/InputHandlers/PropPlacer.cs
+++ Assets/Scripts/InputHandlers/PropPlacer.cs
@@ -1,41 +1,53 @@
 ﻿namespace DLS.LD39.InputHandlers
 {
+    using DLS.LD39.Interface;
     using DLS.LD39.Map;
     using DLS.LD39.Props;
     using UnityEngine;
+    using UnityEngine.UI;
 
     class PropPlacer : MapClickInputHandler
     {
         private enum Mode
         {
             DoNothing,
-            PlaceWall,
-            RemoveWall
+            PlaceProp,
+            RemoveProp
         }
 
+        private Dropdown _selector;
         private Mode _currentMode = Mode.DoNothing;
 
         public PropPlacer() : base("prop", "Prop Placement")
         {
-
+            var selectorObj = GameObject.FindObjectOfType<PropDropdownPopulator>();
+            _selector = selectorObj.GetComponent<Dropdown>();
         }
 
         public override bool HandleButtonDown(int button, Tile clickedTile)
         {
+            var id = _selector.options[_selector.value].text;
+            var propData = PropFactory.Instance.GetPropByID(id);
+            if (propData == null)
+            {
+                return false;
+            }
+            var layer = propData.Layer;
+
             if (button != 0 || _currentMode == Mode.DoNothing)
             {
                 return false;
             }
 
-            var currentProp = clickedTile.PropOnLayer(PropLayer.Wall);
+            var currentProp = clickedTile.PropOnLayer(layer);
 
-            if (_currentMode == Mode.PlaceWall && currentProp == null)
+            if (_currentMode == Mode.PlaceProp && currentProp == null)
             {
-                PropFactory.Instance.BuildPropAndAddToTile("test_wall", clickedTile);
+                PropFactory.Instance.BuildPropAndAddToTile(id, clickedTile);
             }
-            else if (_currentMode == Mode.RemoveWall && currentProp != null)
+            else if (_currentMode == Mode.RemoveProp && currentProp != null)
             {
-                clickedTile.RemoveProp(PropLayer.Wall);
+                clickedTile.RemoveProp(layer);
                 GameObject.Destroy(currentProp.gameObject);
             }
             
@@ -44,14 +56,22 @@
 
         public override bool HandleTileClick(int button, Tile clickedTile)
         {
+            var id = _selector.options[_selector.value].text;
+            var propData = PropFactory.Instance.GetPropByID(id);
+            if (propData == null)
+            {
+                return false;
+            }
+            var layer = propData.Layer;
+
             if (button != 0 || clickedTile == null)
             {
                 return false;
             }
 
-            var prop = clickedTile.PropOnLayer(PropLayer.Wall);
+            var prop = clickedTile.PropOnLayer(layer);
 
-            _currentMode = prop == null ? Mode.PlaceWall : Mode.RemoveWall;
+            _currentMode = prop == null ? Mode.PlaceProp : Mode.RemoveProp;
 
             return false;
         }
