b5085c94eff67e63974420589becd5c16c4c06ed
George Legeza <glegeza@gmail.com>
2017-08-08 12:58:32 -0400

    Populate tile dropdown

---
+++ Assets/Scripts/Interface/TileDropdownPopulator.cs
@@ -0,0 +1,27 @@
+﻿namespace DLS.LD39.Interface
+{
+    using DLS.LD39.Map;
+    using System.Collections.Generic;
+    using UnityEngine;
+    using UnityEngine.UI;
+
+    [RequireComponent(typeof(Dropdown))]
+    class TileDropdownPopulator : MonoBehaviour
+    {
+        private Dropdown _dropdown;
+
+        private void Start()
+        {
+            _dropdown = GetComponent<Dropdown>();
+            _dropdown.ClearOptions();
+
+            var tilemap = FindObjectOfType<TileMap>();
+            var options = new List<string>();
+            foreach (var tile in tilemap.TileData.TileNames)
+            {
+                options.Add(tile);
+            }
+            _dropdown.AddOptions(options);
+        }
+    }
+}
