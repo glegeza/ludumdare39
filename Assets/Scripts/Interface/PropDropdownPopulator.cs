bc4d9e41417267615619cfeb509204580b4e7487
George Legeza <glegeza@gmail.com>
2017-08-08 12:08:02 -0400

    Add populator for props

---
+++ Assets/Scripts/Interface/PropDropdownPopulator.cs
@@ -0,0 +1,27 @@
+﻿namespace DLS.LD39.Interface
+{
+    using System.Collections.Generic;
+    using UnityEngine.UI;
+    using UnityEngine;
+    using DLS.LD39.Props;
+
+    [RequireComponent(typeof(Dropdown))]
+    public class PropDropdownPopulator : MonoBehaviour
+    {
+        private Dropdown _dropdown;
+
+        private void Start()
+        {
+            _dropdown = GetComponent<Dropdown>();
+            _dropdown.ClearOptions();
+
+            var factory = PropFactory.Instance;
+            var options = new List<string>();
+            foreach (var prop in factory.Props)
+            {
+                options.Add(prop.ID);
+            }
+            _dropdown.AddOptions(options);
+        }
+    }
+}
