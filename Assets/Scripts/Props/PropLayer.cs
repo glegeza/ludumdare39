bfea6650de4a4038dad69d5fc68ee27072ae1f45
George Legeza <glegeza@gmail.com>
2017-08-09 15:46:33 -0400

    Add wall decorations layer

--- Assets/Scripts/Props/PropLayer.cs
+++ Assets/Scripts/Props/PropLayer.cs
@@ -1,13 +1,9 @@
 ﻿namespace DLS.LD39.Props
 {
-    using System;
-    using System.Collections.Generic;
-    using System.Linq;
-    using System.Text;
-
     public enum PropLayer
     {
         Floor,
-        Wall
+        Wall,
+        WallDeco
     }
 }
