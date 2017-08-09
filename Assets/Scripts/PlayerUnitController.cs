89abf24c4d9dbf9787f1834cce23e97ddc4ebb88
George Legeza <glegeza@gmail.com>
2017-08-09 00:28:24 -0400

    Add singleton for player unit commands

---
+++ Assets/Scripts/PlayerUnitController.cs
@@ -0,0 +1,38 @@
+﻿namespace DLS.LD39
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using UnityEngine;
+
+    public class PlayerUnitController : SingletonComponent<PlayerUnitController>
+    {
+        private TurnOrderTracker _turnTracker;
+
+        public bool IsPlayersTurn
+        {
+            get
+            {
+                return _turnTracker.ActiveUnit != null &&
+                    _turnTracker.ActiveUnit.Faction == Units.Faction.Player;
+            }
+        }
+
+        public void EndCurrentUnitTurn()
+        {
+            if (!IsPlayersTurn)
+            {
+                return;
+            }
+
+            _turnTracker.AdvanceTurn();
+        }
+
+        private void Start()
+        {
+            _turnTracker = TurnOrderTracker.Instance;
+        }
+    }
+}
