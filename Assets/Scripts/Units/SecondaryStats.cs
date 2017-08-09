1cf8a19ca61239baf9bd127fedc6b4141183cae0
George Legeza <glegeza@gmail.com>
2017-08-09 02:31:00 -0400

    Add SecondaryStats class

---
+++ Assets/Scripts/Units/SecondaryStats.cs
@@ -0,0 +1,42 @@
+﻿namespace DLS.LD39.Units
+{
+    using System;
+
+    public class SecondaryStats
+    {
+        private PrimaryStats _stats;
+
+        public SecondaryStats(PrimaryStats stats)
+        {
+            if (stats == null)
+            {
+                throw new ArgumentNullException("stats");
+            }
+            _stats = stats;
+        }
+
+        public float Initiative
+        {
+            get
+            {
+                return _stats.Speed;
+            }
+        }
+
+        public int ActionPointRegen
+        {
+            get
+            {
+                return _stats.Speed * 3;
+            }
+        }
+
+        public int ActionPointCap
+        {
+            get
+            {
+                return _stats.Speed * 4;
+            }
+        }
+    }
+}
