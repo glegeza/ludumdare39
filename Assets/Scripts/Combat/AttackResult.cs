7a96a0a2b953e314ceb6cbd2c6a4d823a8c52846
George Legeza <glegeza@gmail.com>
2017-08-10 01:19:28 -0400

    Add AttackResult enum

---
+++ Assets/Scripts/Combat/AttackResult.cs
@@ -0,0 +1,11 @@
+﻿namespace DLS.LD39.Combat
+{
+    public enum AttackResult
+    {
+        Hit,
+        Missed,
+        NotEnoughAP,
+        InvalidTarget,
+        InvalidAttackType // unit cannot make melee/ranged attacks
+    }
+}
