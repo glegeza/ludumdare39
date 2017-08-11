e6f5210e629009072f3e1483a7c8a516302e4f3c
George Legeza <glegeza@gmail.com>
2017-08-11 12:35:01 -0400

    Add some random variation to combat text position

--- Assets/Scripts/Interface/FloatingCombatTextController.cs
+++ Assets/Scripts/Interface/FloatingCombatTextController.cs
@@ -6,9 +6,12 @@
     {
         public FloatingCombatText TextPrefab;
         public Canvas UICanvas;
+        public float RandomRange = 0.2f;
 
         public void CreateText(string text, Vector3 location)
         {
+            location += new Vector3(Random.Range(-RandomRange, RandomRange),
+                Random.Range(-RandomRange, RandomRange), 0.0f);
             Vector2 pos = Camera.main.WorldToScreenPoint(location);
             var textObj = Instantiate(TextPrefab, UICanvas.transform, false);
             textObj.transform.position = pos;
