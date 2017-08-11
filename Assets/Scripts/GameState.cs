fb4e4e876482ff5d142b6e721d557226d1be6933
George Legeza <glegeza@gmail.com>
2017-08-11 15:39:34 -0400

    Inherit from SingletonComponent

--- Assets/Scripts/GameState.cs
+++ Assets/Scripts/GameState.cs
@@ -6,22 +6,8 @@
     using System.Text;
     using UnityEngine;
 
-    class GameState : MonoBehaviour
+    public class GameState : SingletonComponent<GameState>
     {
-        private static GameState _instance;
-
-        public static GameState Instance { get { return _instance; } }
-
-        private void Awake()
-        {
-            if (_instance != null && _instance != this)
-            {
-                Destroy(gameObject);
-            }
-            else
-            {
-                _instance = this;
-            }
-        }
+        
     }
 }
