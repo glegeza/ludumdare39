9bcf64fc969feb9d04f6db39124bf9daeb109dd5
George Legeza <glegeza@gmail.com>
2017-08-11 02:26:34 -0400

    Add do nothing state and action

---
+++ Assets/Scripts/AI/Actions/DoNothing.cs
@@ -0,0 +1,13 @@
+﻿namespace DLS.LD39.AI.Actions
+{
+    using UnityEngine;
+
+    [CreateAssetMenu(menuName = "AI/Actions/Do Nothing")]
+    class DoNothing : AIAction
+    {
+        public override bool Act(StateController controller)
+        {
+            return false;
+        }
+    }
+}
