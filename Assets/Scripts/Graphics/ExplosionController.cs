28a1cb553655596e2288e27f1ba8009cdfb2b072
George Legeza <glegeza@gmail.com>
2017-08-11 14:29:30 -0400

    Add singleton component for spawning explosions

---
+++ Assets/Scripts/Graphics/ExplosionController.cs
@@ -0,0 +1,20 @@
+﻿namespace DLS.LD39.Graphics
+{
+    using UnityEngine;
+    using DLS.LD39.Map;
+
+    public class ExplosionController : SingletonComponent<ExplosionController>
+    {
+        public float RandomOffset = 0.3f;
+        public Explosion ExplosionPrefab;
+
+        public void SpawnExplosion(Tile tile)
+        {
+            var pos = tile.WorldCoords;
+            pos += new Vector2(Random.Range(-RandomOffset, RandomOffset),
+                Random.Range(-RandomOffset, RandomOffset));
+            var explosion = Instantiate(ExplosionPrefab);
+            explosion.transform.position = tile.WorldCoords;
+        }
+    }
+}
