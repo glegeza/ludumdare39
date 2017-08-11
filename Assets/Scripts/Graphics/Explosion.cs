1ce138b4a8e2ae7d0c6ef79d1b86036d53936229
George Legeza <glegeza@gmail.com>
2017-08-11 14:29:19 -0400

    Add component to automatically destroy death objects

---
+++ Assets/Scripts/Graphics/Explosion.cs
@@ -0,0 +1,21 @@
+﻿namespace DLS.LD39.Graphics
+{
+    using UnityEngine;
+
+    [RequireComponent(typeof(Animator))]
+    public class Explosion : MonoBehaviour
+    {
+        private Animator _animator;
+
+        private void Awake()
+        {
+            _animator = GetComponent<Animator>();
+        }
+
+        private void Start()
+        {
+            var clipInfo = _animator.GetCurrentAnimatorClipInfo(0)[0];
+            Destroy(gameObject, clipInfo.clip.length);
+        }
+    }
+}
