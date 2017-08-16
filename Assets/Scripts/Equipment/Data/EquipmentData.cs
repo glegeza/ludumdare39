0d405b3ca2b386b0c2aa199614b63a275d271865
George Legeza <glegeza@gmail.com>
2017-08-16 14:02:57 -0400

    Add abstract base equipment data class

---
+++ Assets/Scripts/Equipment/Data/EquipmentData.cs
@@ -0,0 +1,12 @@
+﻿namespace DLS.LD39.Equipment.Data
+{
+    using UnityEngine;
+
+    public abstract class EquipmentData : ScriptableObject
+    {
+        [Header("Equipment Info")]
+        public string ID;
+        public string Name;
+        public string Description;
+    }
+}
