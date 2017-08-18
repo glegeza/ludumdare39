a8e1a90a67d34f73e4c78c47fbf8c99fa1f90f61
George Legeza <glegeza@gmail.com>
2017-08-18 13:42:11 -0400

    Add integer rect and vector2 classes

---
+++ Assets/Scripts/Utility/IntRect.cs
@@ -0,0 +1,127 @@
+﻿namespace DLS.Utility
+{
+    using System;
+    using UnityEngine;
+
+    public class IntRect : IEquatable<IntRect>
+    {
+        private int _x1, _y1, _x2, _y2, _w, _h;
+
+        private IntVector2 _center;
+        private IntVector2 _botLeft;
+        private IntVector2 _topRight;
+        private IntVector2 _topLeft;
+        private IntVector2 _botRight;
+
+        public IntRect(IntVector2 botLeft, int w, int h)
+            : this(botLeft.X, botLeft.Y, w, h)
+        {
+
+        }
+
+        public IntRect(int x, int y, int w, int h)
+        {
+            _x1 = x;
+            _y1 = y;
+            _w = w;
+            _h = h;
+            _x2 = _x1 + _w;
+            _y2 = _y1 + _h;
+
+            _botLeft = new IntVector2(_x1, _y1);
+            _botRight = new IntVector2(_x2, _y1);
+            _topRight = new IntVector2(_x2, _y2);
+            _topLeft = new IntVector2(_x1, _y2);
+        }
+
+        public int X
+        {
+            get
+            {
+                return _x1;
+            }
+        }
+
+        public int Y
+        {
+            get
+            {
+                return _y1;
+            }
+        }
+
+        public IntVector2 Center
+        {
+            get
+            {
+                return new IntVector2(_x1 + Mathf.FloorToInt(_w / 2), _y1 + Mathf.FloorToInt(_h / 2));
+            }
+        }
+
+        public IntVector2 TopLeft
+        {
+            get
+            {
+                return _topLeft;
+            }
+        }
+
+        public IntVector2 TopRight
+        {
+            get
+            {
+                return _topRight;
+            }
+        }
+
+        public IntVector2 BottomLeft
+        {
+            get
+            {
+                return _botLeft;
+            }
+        }
+
+        public IntVector2 BottomRight
+        {
+            get
+            {
+                return _botRight;
+            }
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj == null)
+            {
+                return false;
+            }
+            return Equals(obj as IntRect);
+        }
+
+        public override int GetHashCode()
+        {
+            return SimpleHashBuilder.GetHash(_x1, _y1, _w, _h);
+        }
+
+        public override string ToString()
+        {
+            return String.Format("RectInt: [{0}, {1}] [{2}x{3}]", _x1, _y1, _w, _h);
+        }
+
+        public bool Equals(IntRect other)
+        {
+            return other != null &&
+                _x1 == other._x1 &&
+                _y1 == other._y1 &&
+                _w == other._w &&
+                _h == other._h;
+        }
+
+        public bool Intersects(IntRect other)
+        {
+            return (_x1 < other._x2 && _x2 > other._x1 &&
+                    _y1 < other._y2 && _y2 > other._y1);
+        }
+    }
+}
---
+++ Assets/Scripts/Utility/IntVector2.cs
@@ -0,0 +1,85 @@
+﻿namespace DLS.Utility
+{
+    using UnityEngine;
+    using System;
+
+    [Serializable]
+    public class IntVector2 : IEquatable<IntVector2>
+    {
+        private int _x, _y;
+
+        public IntVector2(int x, int y)
+        {
+            _x = x;
+            _y = y;
+        }
+
+        public IntVector2(float x, float y)
+        {
+            _x = (int)x;
+            _y = (int)y;
+        }
+
+        public int X
+        {
+            get
+            {
+                return _x;
+            }
+        }
+
+        public int Y
+        {
+            get
+            {
+                return _y;
+            }
+        }
+
+        public Vector2 AsVector2()
+        {
+            return new Vector2(_x, _y);
+        }
+
+        public Vector3 AsVector3()
+        {
+            return new Vector3(_x, _y, 0);
+        }
+
+        public override int GetHashCode()
+        {
+            return SimpleHashBuilder.GetHash(_x, _y);
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj == null)
+            {
+                return false;
+            }
+            return Equals(obj as IntVector2);
+        }
+
+        public bool Equals(IntVector2 other)
+        {
+            return other != null &&
+                other._x == _x &&
+                other._y == _y;
+        }
+
+        public override string ToString()
+        {
+            return "IntVector2: (X: " + _x + ", Y: " + _y + ")";
+        }
+
+        public static IntVector2 operator +(IntVector2 a, IntVector2 b)
+        {
+            return new IntVector2(a._x + b._x, a._y + b._y);
+        }
+
+        public static IntVector2 operator -(IntVector2 a, IntVector2 b)
+        {
+            return new IntVector2(a._x - b._x, a._y - b._y);
+        }
+    }
+}
