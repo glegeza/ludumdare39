1baf69fadf90bef030f1a7a9b59b2e43954b1e1c
George Legeza <glegeza@gmail.com>
2017-08-21 15:20:31 -0400

    Add base abstract action class

---
+++ Assets/Scripts/Actions/Action.cs
@@ -0,0 +1,26 @@
+﻿namespace DLS.LD39.Actions
+{
+    using DLS.LD39.Map;
+    using DLS.LD39.Units;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using UnityEngine;
+
+    public abstract class Action : ScriptableObject
+    {
+        public string ID;
+        public Sprite IconSprite;
+
+        public abstract string GetName();
+
+        public abstract string GetDescription();
+
+        public abstract int GetAPCost();
+
+        public abstract bool ActionIsValid(GameUnit actor, GameObject target, Tile targetTile);
+
+        public abstract bool AttemptAction(GameUnit actor, GameObject target, Tile targetTile);
+    }
+}
