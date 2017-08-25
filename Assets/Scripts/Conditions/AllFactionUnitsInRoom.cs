573bf72b697f9457f45f4e0ec0346feca8340e68
George Legeza <glegeza@gmail.com>
2017-08-25 15:08:28 -0400

    Add more generic room check

---
+++ Assets/Scripts/Conditions/AllFactionUnitsInRoom.cs
@@ -0,0 +1,34 @@
+﻿namespace DLS.LD39.Conditions
+{
+    using System.Linq;
+    using Units;
+    using UnityEngine;
+
+    [CreateAssetMenu(menuName = "Conditions/All Units In Room")]
+    public class AllFactionUnitsInRoom : GameCondition
+    {
+        public string RoomID;
+        public Faction Faction;
+
+        public override ConditionCheckInterval Interval
+        {
+            get { return ConditionCheckInterval.TurnEnd; }
+        }
+
+        public override bool IsConditionMet()
+        {
+            var room = GameState.Instance.CurrentMap.GetRoom(RoomID);
+            var totalUnits = 0;
+            foreach (var unit in ActiveUnits.Instance.Units.Where(u => u.Faction == Faction))
+            {
+                totalUnits += 1;
+                if (!room.UnitInRoom(unit))
+                {
+                    return false;
+                }
+            }
+
+            return totalUnits > 0;
+        }
+    }
+}
